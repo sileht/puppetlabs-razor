e05704d2255b67eb7cd29633c960ec320e70d2d2
Mehdi Abaakouk <sileht@sileht.net>
2012-08-27 18:42:44 +0200

    Write spec to match config file generation

--- spec/classes/razor_spec.rb
+++ spec/classes/razor_spec.rb
@@ -8,7 +8,8 @@ describe 'razor', :type => :class do
       :persist_host        => '127.0.0.1',
       :mk_checkin_interval => '60',
       :git_source          => 'http://github.com/johndoe/Razor.git',
-      :git_revision        => '1ef7d2'
+      :git_revision        => '1ef7d2',
+      :server_opts_hash    => { 'mk_log_level' => 'Logger::DEBUG' },
     }
   end
 
@@ -36,9 +37,6 @@ describe 'razor', :type => :class do
           :directory => params[:directory]
         )
         should include_class('razor::tftp')
-        should contain_file("#{platform[:path]}/razor.ipxe").with(
-          :content => /http:\/\/#{facts[:ipaddress]}:8026\/razor\/api\/boot\?hw_id=\$\{net0\/mac\}/
-        )
         should include_class('razor::ruby')
         should contain_user(params[:username]).with(
           :ensure => 'present',
@@ -74,14 +72,30 @@ describe 'razor', :type => :class do
           :require   => ['Class[Mongodb]', 'File[/var/lib/razor]', 'Sudo::Conf[razor]'],
           :subscribe => ['Class[Razor::Nodejs]', "Vcsrepo[#{params[:directory]}]"]
         )
-        should contain_file("#{params[:directory]}/conf/razor_server.conf").with(
-          :ensure  => 'file',
+        should contain_exec('get_default_config').with_command("#{params[:directory]}/bin/razor config default | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*\$//g' -e 's/:\$/: \"\"/g' -e 's/persist_mode: /&:/' | grep -v -e '^ProjectRazor Config' -e '^image_svc_host:' -e '^image_svc_path:' -e '^mk_checkin_interval:' -e '^mk_log_level:' -e '^mk_uri:' -e '^persist_host:' | sort > #{params[:directory]}/conf/razor_server.conf.default")
+        should include_class('concat::setup')
+        should contain_concat__fragment("razor_server.conf.default").with(
+          #:require => 'Exec[get_default_config]', # disable due to "razor config default" workaround
+          :source => "#{params[:directory]}/conf/razor_server.conf.default"
+        )
+        should contain_concat__fragment("razor_server.conf.custom").with(
           :content => /image_svc_host: #{facts[:ipaddress]}/,
           :content => /image_svc_path: #{params[:directory]}\/image/,
           :content => /mk_uri: http:\/\/#{facts[:ipaddress]}:8026/,
           :content => /mk_checkin_interval: #{params[:mk_checkin_interval]}/,
           :content => /persist_host: #{params[:persist_host]}/,
+          :content => /mk_log_level: #{params[:server_opts_hash]['mk_log_level']}/
+        )
+        should contain_concat("#{params[:directory]}/conf/razor_server.conf").with(
           :notify  => 'Service[razor]'
+        )
+        should contain_exec('gen_ipxe').with_command("#{params[:directory]}/bin/razor config ipxe > /tmp/razor.ipxe")
+        should contain_exec('gen_ipxe').with(
+          :subscribe => "File[#{params[:directory]}/conf/razor_server.conf]"
+        )
+        should contain_file("#{platform[:path]}/razor.ipxe").with(
+          :source => '/tmp/razor.ipxe',
+          :subscribe => 'Exec[gen_ipxe]'
         )
       }
     end
