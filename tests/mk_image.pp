c1325969af3476de88436fbf4053baafe5b511c9
Nan Liu <nan@puppetlabs.com>
2012-09-19 15:27:06 -0700

    Fix mk image to use dev for 0.9.1.4

--- tests/mk_image.pp
+++ tests/mk_image.pp
@@ -5,8 +5,8 @@ rz_image { 'rz_mk_prod-image.0.9.0.4.iso':
   source  => 'https://github.com/downloads/puppetlabs/Razor-Microkernel/rz_mk_prod-image.0.9.0.4.iso',
 }
 
-rz_image { 'rz_mk_prod-image.0.9.1.4.iso':
+rz_image { 'rz_mk_dev-image.0.9.1.4.iso':
   ensure  => present,
   type    => 'mk',
-  source  => 'https://github.com/downloads/puppetlabs/Razor-Microkernel/rz_mk_prod-image.0.9.1.4.iso',
+  source  => 'https://github.com/downloads/puppetlabs/Razor-Microkernel/rz_mk_dev-image.0.9.1.4.iso',
 }
